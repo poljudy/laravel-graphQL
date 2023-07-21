2526df3263b58d17085eb6a07b1fe4752386c378
poljudy <franklinjin229@gmail.com>
2023-07-21 12:50:34 -0700

    updated graphQL types

--- app/GraphQL/Types/SkillType.php
+++ app/GraphQL/Types/SkillType.php
@@ -11,21 +11,21 @@ use Rebing\GraphQL\Support\Type as GraphQLType;
 class SkillType extends GraphQLType
 {
     protected $attributes = [
-        'name'              => 'skill',
-        'description'       => 'A type of skills',
-        'model'             => Skill::class,
+        'name' => 'skill',
+        'description' => 'A type of skills',
+        'model' => Skill::class,
     ];
 
     public function fields(): array
     {
         return [
-            'id'                => [
-                'type'          => Type::nonNull(Type::int()),
-                'description'   => "The id field"
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => "The id field"
             ],
-            'skill_title'       => [
-                'type'          => Type::string(),
-                'description'   => "The skill_title of the skills"
+            'skill_title' => [
+                'type' => Type::string(),
+                'description' => "The skill_title of the skills"
             ]
         ];
     }
